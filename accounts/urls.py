fdc29935c2d36618e7824393c88842b5397c3479
Hearen <630897810@qq.com>
2017-08-08 23:29:00 +0800

    Add password_change function

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth.views import (
     password_reset,
     password_reset_done,
     logout,
+    password_change,
+    password_change_done,
 )
 
 urlpatterns = [
@@ -14,4 +16,6 @@ urlpatterns = [
     url(regex=r'^password/reset/done/$', view=password_reset_done, name='password_reset_done'),
     url(regex=r'^signup/', view=SignUpView.as_view(), name='signup',),
     url(regex=r'^logout/$', view=logout, kwargs={'next_page': '/accounts/login/'}, name='logout'),
+    url(regex=r'^password/change/$', view=password_change, name='password_change'),
+    url(regex=r'^password/change/done/$', view=password_change_done, name='password_change_done'),
 ]
